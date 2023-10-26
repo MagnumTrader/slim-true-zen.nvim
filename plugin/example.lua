30060382e5c1fa10ae50ca922c8d21cb41a80265
jimmy <jimmy.w.larsson@gmail.com>
2023-10-26 17:45:03 +0200

    layout implementerad custom size split fungerar

--- plugin/example.lua
+++ plugin/example.lua
@@ -1,102 +1,143 @@
 -- this gets loaded on the import
 M = {
-    num = 0
-    -- keep center window and buffer id.
-    -- keep pref state
-    -- keep the  state of lualine
+    focus_width = 0.4,
+    windows = {},
+    tab = {},
 }
+-- TODO här kan jag se om jag ska öppna upp den igen 
+M.running = false
 
--- TODO skapa buffrar som inte är modifierbara
-function In(v)
-        print(vim.inspect(v))
+local opt = vim.opt
+local api = vim.api
+local cmd = vim.cmd
+
+local function add_padding(command, props, move)
+    -- leftabove if vnew = left, new = abpve
+    -- rightbelow if vnew = right, new = below
+    -- splitting, focus is now on padded window
+    cmd(command)
+
+    local new_window = vim.api.nvim_get_current_win()
+    if  props.height ~= nil then
+        api.nvim_win_set_height(0, props.height)
+    end
+    if props.width ~= nil then
+        api.nvim_win_set_width(0, props.width)
+    end
+
+    -- set the width of that buffer
+    opt.number = false
+    opt.relativenumber = false
+    opt.modified = false
+    opt.cursorline = false
+    -- api.nvim_buf_set_option(api.nvim_get_current_buf(), "readonly", true)
+    opt.fillchars = { eob = ' ', vert = ' ' }
+    -- win.left = pad_win("leftabove vnew", { width = left_padding }, "wincmd l") -- left buffer
+
+    cmd(move)
+
+    return new_window
 end
-local function split_window(window, direction)
-    vim.api.nvim_set_current_win(window)
-    vim.cmd('vsplit')
-end
-local function resize_window(measurements)
-    measurements.height = 1;
-end
+
 local f = function()
 
+    local old_fillchars = opt.fillchars
+
+    -- disable some keymaps that will interfere with the plugin
+
+    -- set cursorline!
+    -- set nonumber
+    -- set norelativenumber
+--[[
+options from true-zen config for buffer
+local opts = {
+	bo = {
+		buftype = "nofile",
+		bufhidden = "hide",
+		modifiable = false,
+		buflisted = false,
+		swapfile = false,
+	},
+	wo = {
+		cursorline = false,
+		cursorcolumn = false,
+		number = false,
+		relativenumber = false,
+		foldenable = false,
+		list = false,
+	},
+}
+
+        detta används idag, kommando först för att splitta, sedan också ett kommande
+        för att gå tillbaka till mitten 
+        win.left = pad_win("leftabove vnew", { width = left_padding }, "wincmd l") -- left buffer
+        win.right = pad_win("vnew", { width = right_padding }, "wincmd h") -- right buffer
+        win.top = pad_win("leftabove new", { height = top_padding }, "wincmd j") -- top buffer
+        win.bottom = pad_win("rightbelow new", { height = bottom_padding }, "wincmd k") -- bottom buffer
+--]]
+
+
+
     -- TODO how can i save all the old settings here to be reset later?
-    local prevsplitright = vim.opt.splitright
-    local prevsplitbelow = vim.opt.splitbelow
+    local prevsplitright = opt.splitright
+    local prevsplitbelow = opt.splitbelow
+
+    opt.splitright = true
+    opt.splitbelow = true
+
 
     -- save the buffer
-    local focused_buf = vim.api.nvim_get_current_buf()
+    local focused_buf = api.nvim_get_current_buf()
 
     -- create a new tab that we use 
-    vim.cmd("tabnew")
-    -- regular split, 
-    vim.opt.splitright = false
-    local empty_created_buf = vim.api.nvim_win_get_buf(vim.api.nvim_get_current_win())
-    vim.cmd('vsplit')
-    -- insert buffer to window
-    -- split with empty buf
+    cmd("tabnew")
 
-    -- verkar vara en setting om vilket håll jag ska splitta åt , kan använda denn inställning för att centrera
+    print(vim.api.nvim_win_get_width(0) * 0.6)
 
-    vim.opt.splitright = true
-    vim.cmd('vsplit')
+    M.windows.left = add_padding("leftabove vsplit", {width = 60}, "wincmd l")
+    M.windows.right = add_padding("rightbelow vsplit", {width = 30}, "wincmd h")
+    M.windows.top = add_padding("leftabove split", {height = 2}, "wincmd j")
+    M.windows.bottom = add_padding("rightbelow split", {height = 2}, "wincmd k")
 
-    local curr_win = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(curr_win, focused_buf)
+    local curr_win = api.nvim_get_current_win()
+    api.nvim_win_set_buf(curr_win, focused_buf)
 
-
+    cmd("")
 
     --TODO graceful shutdown, buffers ska förstöras efter här ?
-       -- remap :q to shutdown the view
+    -- remap :q to shutdown the view
     -- close the window im focusing
     -- delete open buffers
+        -- close current buffer that is used.
+        -- list all buffers in tab, delete them
     -- close the tab
 
 
-    -- vim.api.nvim_buf_delete(empty_created_buf, {})
+    -- api.nvim_buf_delete(empty_created_buf, {})
 
 
 
     -- resets all the old settings global settings
 
-    vim.opt.splitright = prevsplitright
-    vim.opt.splitbelow = prevsplitbelow
+    opt.splitright = prevsplitright
+    opt.splitbelow = prevsplitbelow
+
+
 end
 
-function M.reset_m()
-    M.num = 0
-    print(M.asd)
-end
 
+local function del_buffers_in_cur_tab()
+    local windows = vim.api.nvim_tabpage_list_wins(0)
+    for _, window in pairs(windows) do
+        vim.api.nvim_buf_delete(vim.api.nvim_win_get_buf(window), {})
+        print(vim.api.nvim_win_get_buf(window))
+    end
+end
 
 vim.keymap.set('n', 'gz', f)
-vim.keymap.set('n', 'gt', M.reset_m)
+
+function M.toggle()
+    
+end
+
 return M
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
