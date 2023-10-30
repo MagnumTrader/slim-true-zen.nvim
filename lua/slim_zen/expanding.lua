2e3d43cf1e9d5dd9421be828979bae8fe7c9496a
Jimmy Larsson <jimmy.w.larsson@gmail.com>
2023-10-30 09:30:52 +0100

    update

--- lua/slim_zen/expanding.lua
+++ lua/slim_zen/expanding.lua
@@ -5,13 +5,25 @@ local simple = require('slim_zen.simple_zen')
 local M = {}
 
 M.running = false
+-- field to check if expanding mode triggered the first simple
 M.simple_toggled = false
 
+-- TODO should these be On and off functions instead?
 function M.toggle(line1, line2)
 
+    -- BUG when being in simple mode. then triggering expanding.
+    -- then using simple mode toggle, we get back to the original layout 
+    -- origin -> simple -> expanding -> (trigger simple mode takes us to) origin, when it should be simple
+    -- maybe running shouldnt be tied to toggle
+
+
+
+    -- TODO it can actually take range here since we havent started filtering the buffer yet, 
+    -- or called simple wich are doing all the padding 
     if not simple.running then
         simple.toggle()
         M.simple_toggled = true
+        -- hejsan jag skriver massa kommentarer 
     end
 
     if M.running then
@@ -20,17 +32,24 @@ function M.toggle(line1, line2)
             simple.toggle()
             M.simple_toggled = false
         end
+        cmd(':normal! zz')
         M.running = false
 
         return
     end
 
     local cursor = api.nvim_win_get_cursor(0)
-    api.nvim_win_set_cursor(0 , {line1 -1, 0})
-    cmd(":normal! zfgg")
 
-    api.nvim_win_set_cursor(0 , {line2 + 1, 0})
-    cmd(":normal! zfG")
+    print(cursor[1])
+    if cursor[1] ~= 1 then
+        api.nvim_win_set_cursor(0 , {line1 -1, 0})
+        cmd(":normal! zfgg")
+    end
+
+    if cursor[1] ~= api.nvim_buf_line_count(0) then
+        api.nvim_win_set_cursor(0 , {line2 + 1, 0})
+        cmd(":normal! zfG")
+    end
 
     api.nvim_win_set_cursor(0, cursor) -- TODO this should be middle afterwards
 
--- lua/slim_zen/simple_zen.lua
+++ lua/slim_zen/simple_zen.lua
@@ -1,63 +1,16 @@
+local opt = vim.opt
+local api = vim.api
+local cmd = vim.cmd
+
+local add_padding = require('slim_zen.utils').add_padding
+
+
 M = {
     focus_width = 0.4,
     windows = {},
     tab = {},
 }
 M.running = false
-
-local opt = vim.opt
-local api = vim.api
-local cmd = vim.cmd
-
--- TODO move to utils
--- splitting and adding the padding to a window
--- Returning the new windows identifying integer
---
--- commands:
--- "leftabove [newtype]" (vnew = left, new = above)
--- "rightbelow [newtype]" (vnew = right, new = below)
--- 
--- props:
--- height or width alloed. specifies how large the padding window should be.
--- no error handling, use width for vertical splits and height for horizontal
---
--- move:
--- Move to be made after window is created, 
--- created left, move right with "wincmd l"
--- created top, move down with "wincmd j"
--- created right, move left with "wincmd h"
--- created bottom, move up with "wincmd k"
--- TODO. set init window variable to M, then we just move back to it without specifying move commands
---
--- example adding padding to the left and above
--- M.windows.left = add_padding("leftabove vsplit", {width = 60}, "wincmd l")
--- M.windows.right = add_padding("rightbelow vsplit", {width = 60}, "wincmd h")
--- M.windows.top = add_padding("leftabove split", {height = 2}, "wincmd j")
--- M.windows.bottom = add_padding("rightbelow split", {height = 2}, "wincmd k")
---
---
--- returns the created window id
--- Credit, insperation for function: https://github.com/pocco81/true-zen.nvim
-local function add_padding(command, props, move)
-
-
-    -- TODO if focus_props exists, utgå ifrån den och padda allt runt omkring
-    -- annars låt användaren padda alla enskilda 
-    cmd(command) -- creates the new split
-    if  props.height ~= nil then
-        api.nvim_win_set_height(0, props.height)
-    end
-    if props.width ~= nil then
-        api.nvim_win_set_width(0, props.width)
-    end
-
-    local new_window = vim.api.nvim_get_current_win()
-    -- moves back to the intended focus window
-    cmd(move)
-
-    return new_window
-
-end
 
 
 -- creating the layout for my zen mode 
